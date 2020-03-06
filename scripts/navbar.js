2646dc9f36deeffe0cab4c9b8488646760d6d6e7
nrcelik <59237286+nrcelik@users.noreply.github.com>
2020-03-06 21:21:33 +0000

    Added a few features

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -7,3 +7,26 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+/* Add sticky feature to the nav */
+$(document).ready(function() {
+  var stickyNavTop = $('.topnav').offset().top;
+
+  var stickyNav = function(){
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > stickyNavTop) { 
+      $('.topnav').addClass('sticky');
+    } else {
+      $('.topnav').removeClass('sticky'); 
+    }
+  };
+
+  stickyNav();
+
+  $(window).scroll(function() {
+    stickyNav();
+  });
+});
+ 
+
